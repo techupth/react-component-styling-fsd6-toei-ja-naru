e638ff57e816e1f27da1c5411b6ba28a1784d289
Ja <ja.techup@gmail.com>
2024-06-04 16:31:22 +0700

    feat: Component Styling Assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,46 @@
 import "./App.css";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+import error from "./components/alert-icons/error.png";
+import warning from "./components/alert-icons/warning.png";
+import info from "./components/alert-icons/info.png";
+import success from "./components/alert-icons/success.png";
 
 function App() {
   return (
     <div className="App">
       <div className="button-components-section">
         {/* Render ตัว Button 2 แบบ */}
+        <Button type="primary" />
+        <Button type="secondary" />
       </div>
       <hr />
       <div className="alert-components-section">
         {/* Render ตัว Alert 4 แบบ */}
+        <Alert
+          alt="Error"
+          text="This is error alert box"
+          bgColor="#F9C8C8"
+          icon={error}
+        />
+        <Alert
+          alt="Warning"
+          text="This is warning alert box"
+          bgColor="#F9D9C8"
+          icon={warning}
+        />
+        <Alert
+          alt="Info"
+          text="This is info alert box"
+          bgColor="#F9EBC8"
+          icon={info}
+        />
+        <Alert
+          alt="Success"
+          text="This is success alert box"
+          bgColor="#CEF7CD"
+          icon={success}
+        />
       </div>
     </div>
   );
--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,3 +1,36 @@
 // Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 
-function Alert() {}
+function Alert(props) {
+  return (
+    <div
+      css={css`
+        display: flex;
+        width: 650px;
+        height: auto;
+        font-size: 13px;
+        color: #444444;
+        background-color: ${props.bgColor};
+        padding: 7px 30px;
+        gap: 18px;
+        align-item: center;
+        border-radius: 10px;
+        margin: 30px;
+      `}
+    >
+      <img
+        src={props.icon}
+        alt={props.alt}
+        css={css`
+          width: 24px;
+          height: 24px;
+          align-self: center;
+        `}
+      />
+      <h2>{props.text}</h2>
+    </div>
+  );
+}
+
+export default Alert;
