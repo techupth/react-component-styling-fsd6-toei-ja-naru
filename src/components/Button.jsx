ab8bd5886af90a9be7694f5384d940c483fb3179
Puttapong <puttapong.lekk@gmail.com>
2024-06-04 16:38:17 +0700

    feat:Add button and alert

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,62 @@
-// Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 
-function Button() {}
+function Button(prop) {
+  if (prop.text === "Primary") {
+    return (
+      <section
+        css={css`
+          width: 331px;
+          height: 50px;
+          margin: 20px;
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          font-size: 20px;
+        `}
+      >
+        <p>Primary</p>
+        <button
+          css={css`
+            width: 171px;
+            height: 50px;
+            border-radius: 4px;
+            background-color: #074ee8;
+            font-size: 16px;
+          `}
+        >
+          Button
+        </button>
+      </section>
+    );
+  } else if (prop.text === "Secondary") {
+    return (
+      <section
+        css={css`
+          width: 331px;
+          height: 50px;
+          margin: 20px;
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          font-size: 20px;
+        `}
+      >
+        <p>Secondary</p>
+        <button
+          css={css`
+            width: 171px;
+            height: 50px;
+            border-radius: 4px;
+            background-color: #07a4e8;
+            font-size: 16px;
+          `}
+        >
+          Button
+        </button>
+      </section>
+    );
+  }
+}
+
+export default Button;
