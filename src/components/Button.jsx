ae6063123ba5d34894f7b8d11ff599702b15fce0
Ja <ja.techup@gmail.com>
2024-06-04 16:52:43 +0700

    refactor: button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,67 +3,32 @@
 import { css } from "@emotion/react";
 
 function Button(props) {
-  // let bgColor = "#074EE8";
-  if (props.type === "primary") {
-    return (
-      <button
-        css={css`
-          background-color: #074ee8;
-          font-size: 16px;
-          color: white;
-          height: 50px;
-          width: 172px;
-          padding: 0px, px, 0px, 16px;
-          radius: 4px;
-          display: block;
-          margin: 20px;
-          margin-left: auto;
-          margin-right: auto;
-        `}
-      >
-        Button
-      </button>
-    );
-  } else if (props.type === "secondary") {
-    return (
-      <button
-        css={css`
-          background-color: #07a4e8;
-          font-size: 16px;
-          color: white;
-          height: 50px;
-          width: 172px;
-          padding: 0px, px, 0px, 16px;
-          radius: 4px;
-          display: block;
-          margin: 20px;
-          margin-left: auto;
-          margin-right: auto;
-        `}
-      >
-        Button
-      </button>
-    );
-  }
-}
+  let bgColor = "";
 
-// return (
-//   <button
-//     css={css`
-//       background-color: ${bgColor};
-//       font-size: 16px;
-//       color: white;
-//       height: 50px;
-//       width: 172px;
-//       padding: 0px, px, 0px, 16px;
-//       radius: 4px;
-//       display: block;
-//       margin: 20px;
-//     `}
-//   >
-//     Button
-//   </button>
-// );
-// }
+  if (props.type === "primary") {
+    bgColor = "#074EE8";
+  } else if (props.type === "secondary") {
+    bgColor = "#07a4e8";
+  }
+  return (
+    <button
+      css={css`
+        background-color: ${bgColor};
+        font-size: 16px;
+        color: white;
+        height: 50px;
+        width: 172px;
+        padding: 0px, px, 0px, 16px;
+        radius: 4px;
+        display: block;
+        margin: 20px;
+        margin-left: auto;
+        margin-right: auto;
+      `}
+    >
+      Button
+    </button>
+  );
+}
 
 export default Button;
